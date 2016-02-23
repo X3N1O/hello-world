718b4da66ef9a09f0b7af8e96a37d01e312e1e65
X3N1O <xxenio99x@gmail.com>
2016-02-23 13:11:35 +0100

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World");
+  return 0;
+}
+
+/*
+* The #include includes the headerfile for standart input/output stuff such as printf() and scanf()
+* int main() creates a function, the Main-Function which is needed in every C-Program
+* printf() prints a STRING, a couple of letters, numbers etc. ( careful with the \-sign/backslash )
+* every C-Program has to return some value. This returns 0, which means that the program ran successfully
+*
+*/
